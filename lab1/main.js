84deb4794d1925cdc2b5ea610d4794d55deaae6b
jStalbrand <gitlab.student.lnu.se>
2020-11-09 18:30:33 +0100

    lab2

---
+++ lab1/main.js
@@ -0,0 +1,22 @@
+
+var arr = [];
+for(var i = 0; i < 10; i++){
+    arr.push(i);
+}
+
+function init(event){
+
+    var a = get_random_nr();
+    var b = get_random_nr();
+    document.querySelector("#page-wrapper").innerHTML = a + " + " + b + " = " + (a + b);
+
+}
+
+function get_random_nr() {
+    
+    var i = Math.floor(Math.random() * 9);
+    
+	return Number.parseInt(arr.splice(i, 1));
+}
+
+window.onload = init;
