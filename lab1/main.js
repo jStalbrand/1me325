a52f820947c4374547fa4d43fd69f79c99d762ec
jStalbrand <gitlab.student.lnu.se>
2020-11-10 13:28:58 +0100

    ffsdffs

--- lab1/main.js
+++ lab1/main.js
@@ -6,6 +6,9 @@ for(var i = 0; i < 10; i++){
 console.log("dasdasda");
 console.log("dasdasda");
 console.log("dasdasda");
+console.log("dasdasda");
+console.log("dasdasda");
+console.log("dasdasda");
 
 console.log(arr);
 
