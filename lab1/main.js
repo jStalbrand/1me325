346321e55bd14eb0e53192798b2ec7c9007fb275
jStalbrand <gitlab.student.lnu.se>
2020-11-10 14:10:05 +0100

    senaste versionen

--- lab1/main.js
+++ lab1/main.js
@@ -1,25 +1,2 @@
-
-var arr = [];
-for(var i = 0; i < 10; i++){
-    arr.push(i);
-}
-console.log(arr);
-
-function init(event){
-
-    var a = get_random_nr();
-    var b = get_random_nr();
-    var z = a + b;
-    document.querySelector("#page-wrapper").innerHTML = a + " + " + b + " = " + (a + b);
-
-}
-
-function get_random_nr() {
-    
-    var i = Math.floor(Math.random() * 9);
-    
-	return Number.parseInt(arr.splice(i, 1));
-}
-
-window.onload = init;
-
+console.log("hello world");
+console.log("hello world");
